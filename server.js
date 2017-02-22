6f9d3f220b5f51462d58ae4091e0aa458dc76e94
Stephen Ward <srmward@gmail.com>
2017-02-22 09:38:57 -0800

    added first view, crud operations in controller

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const Path = require('path');
 const Routes = require('./lib/routes');
 const Hapi = require('hapi');
 const Hoek = require('hoek');
@@ -9,7 +10,24 @@ const Models = require('./lib/models/');
 const server = new Hapi.Server();
 server.connection({ port: Settings.port });
 
-server.route(Routes);
+server.register([
+  require('vision')
+], (err) => {
+  Hoek.assert(!err, err);
+
+  // View settings
+  server.views({
+    engines: { pug: require('pug') },
+    path: Path.join(__dirname, 'lib/views'),
+    compileOptions: {
+      pretty: false
+    },
+    isCached: Settings.env === 'production'
+  });
+
+  // Add routes
+  server.route(Routes);
+});
 
 Models.sequelize.sync().then(() => {
   server.start((err) => {
